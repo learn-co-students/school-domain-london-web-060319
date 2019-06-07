f81182532749cc66b5a4cd923f93fddb3dbdbdfb
Louise Norris <louisenorris73@yahoo.co.uk>
2019-06-07 21:03:55 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+
+    attr_accessor :name, :roster, :class, :grade
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+    def add_student(student, grade)
+      roster[grade] ||= []
+      roster[grade] << student
+    #  binding.pry
+    end
+
+    def grade(grade)
+      roster[grade]
+    end
+
+    def sort
+      sorted_students = {}
+      roster.each do |grade, students|
+        sorted_students[grade] = students.sort
+      end
+      sorted_students
+    end
+
+end
